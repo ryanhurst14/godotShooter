40199878c8600aaaa21ba6b2142f2d1807bcd1e7
ryanhurst14 <ryanhurst1017@gmail.com>
2025-06-21 15:13:53 +1000

    Added player input

---
+++ scenes/levels/level.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var test_array: Array[String] = ["a", "b", "c"]
+
+func _ready():
+	$Logo.rotation_degrees = 90
+	
+	print(test_array[0])
+
+
+func _process(delta):
+	$Logo.rotation_degrees += 60*delta
+	
+	if $Logo.position.x > 1000:
+		$Logo.pos.x = 0
+		
+
+		
+	
+	
+		
+		
