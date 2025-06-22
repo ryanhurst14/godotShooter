f8674e5c69f904fade131071e2425f575d061bce
ryanhurst14 <ryanhurst1017@gmail.com>
2025-06-22 20:20:17 +1000

    Gun now shoots a laser and grenade partially functionable

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -2,10 +2,20 @@ extends Node2D
 
 var test_array: Array[String] = ["a", "b", "c"]
 
+var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
+var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
+func _on_gate_player_entered_gate(body) -> void:
+	print("player has entered gate")
+	print(body)
 
-func _on_area_2d_body_entered(_body: Node2D) -> void:
-	print("Etnett")
+func _on_player_laser(pos) -> void:
+	var laser = laser_scene.instantiate()
+	laser.position = pos
+	$Projectiles.add_child(laser)
 
 
-func _on_area_2d_body_exited(_body: Node2D) -> void:
-	print("GONE")
+func _on_player_grenade(pos) -> void:
+	var grenade = grenade_scene.instantiate()
+	grenade.position = pos
+	$Projectiles.add_child(grenade)
+	
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 var can_laser = true
 var can_grenade = true
 
+signal laser(pos)
+signal grenade(pos)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,22 +21,26 @@ func _process(_delta: float) -> void:
 	
 	#Laser shooting input
 	if Input.is_action_pressed("primary action") and can_laser:
-		print("Shoot")
+		var laser_markers = $LaserStartPositions.get_children()
+		var selected_laser = laser_markers[randi() % laser_markers.size()]
 		can_laser = false
 		$Timer.start()
+		laser.emit(selected_laser.global_position)
+		
 		
 		
 	if Input.is_action_pressed("secondary action") and can_grenade:
 		print("Grenade")
 		can_grenade = false
 		$GrenadeTimer.start()
+		var pos_marker = $LaserStartPositions.get_children()[0].global_position
+		grenade.emit(pos_marker)
 		
 
 
 
 func _on_timer_timeout() -> void:
 	can_laser = true
-	
 	
 
 
